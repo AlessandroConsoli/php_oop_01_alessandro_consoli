da96d0d8f801dc70c602e38c2f9711aa0c6dc32d
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-11-14 18:15:17 +0100

    completato

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ class Company {
     public $name;
     public $location;
     public $tot_employees = 0;
+    public static $salary = 2000;
+    public static $total = 0;
 
 
 
@@ -25,8 +27,23 @@ class Company {
     }
 
 
-    public function annualExpences(){
-        echo "Il costo annuale dei dipendenti per l'azienda $this->name è" . " " . $this->tot_employees * 2000 * 12;
+    public function calculateAnnualExpences($int){
+        return $this->tot_employees * (self::$salary * $int);
+    }
+
+
+    public function printAnnualExpences($month = 12){
+        echo "-----$this->name-----\n";
+        echo "Il costo annuale dei dipendenti per l'azienda $this->name è di " . $this->calculateAnnualExpences($month) . " euro \n";
+    }
+
+
+    public function calculateProgressiveCost($month = 12){
+        return self::$total += $this->calculateAnnualExpences($month);
+    }
+
+    public static function printCalculatedFinalTotal(){
+        echo "Il totale delle spese di tutte le aziende analizzate è pari a: " . self::$total . " \n";
     }
 
 }
@@ -39,8 +56,27 @@ $azienda4 = new Company('Maserati', 'Viale Vittorio Veneto 9 Catania,', 150);
 $azienda5 = new Company('Nissan', 'Corso Italia 110 Catania,', 70);
 
 
+$azienda1->printAnnualExpences();
 $azienda1->employeesCounter();
-$azienda1->annualExpences();
+$azienda1->calculateProgressiveCost();
+
+$azienda2->printAnnualExpences();
+$azienda2->employeesCounter();
+$azienda2->calculateProgressiveCost();
+
+$azienda3->printAnnualExpences();
+$azienda3->employeesCounter();
+$azienda3->calculateProgressiveCost();
+
+$azienda4->printAnnualExpences();
+$azienda4->employeesCounter();
+$azienda4->calculateProgressiveCost();
+
+$azienda5->printAnnualExpences();
+$azienda5->employeesCounter();
+$azienda5->calculateProgressiveCost();
+
+Company::printCalculatedFinalTotal();
 
 
 
